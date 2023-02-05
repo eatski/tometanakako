6084e3e2447450aeab599214173be6f37627b64a
hagaitski <i.haga629@gmail.com>
2023-02-05 12:51:45 +0900

    hello openai

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,30 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-
-type Data = {
-  name: string
-}
+import type { NextApiResponse } from 'next'
+import { Configuration, OpenAIApi } from "openai";
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
 export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
+  _: never,
+  res: NextApiResponse<any>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: "Q: やあやあ、こんにちは！ \nA:",
+    temperature: 0,
+    top_p: 1,
+    frequency_penalty: 0.0,
+    presence_penalty: 0.0,
+    stop: ["\n"],
+  }).then(e => {
+    res.status(200).json({
+      answer:e.data.choices[0].text
+    })
+  }).catch(() => {
+    res.status(500).json({
+      error: "OpenAI API Error"
+    })
+  })
 }
