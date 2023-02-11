4bf010c6a5d9fba0218df43841ca7bb46818a770
hagaitski <i.haga629@gmail.com>
2023-02-11 22:10:28 +0900

    usecaseに外だし

--- src/pages/api/stories/[story]/correct.ts
+++ src/pages/api/stories/[story]/correct.ts
@@ -1,60 +1,23 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { getStory } from '@/models/getStory';
-import type { NextApiHandler } from 'next'
-import { Configuration, OpenAIApi } from "openai";
+import { checkAnswerIsCorrect } from '@/usecases/correct';
+import type { NextApiHandler } from 'next';
+import * as z from "zod"
 
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
+const RequestBody = z.object({
+    prompt: z.string(),
+    debugMode: z.boolean()
+})
 
 const handler : NextApiHandler = async (req, res) => {
     if(!(typeof req.query.story === "string")){
         throw new Error("Invalid query")
     };
-    const story = await getStory(req.query.story);
-    const body = JSON.parse(req.body);
-    const prompt = `
-# 概要
-問題とそれの答え、及びそれを読んだ回答者の回答を見て出力を決めてください。
-
-# 問題
-${story.question}
-
-# 問題の答え
-${story.coreDescription}
-
-# 回答者の回答
-${body.prompt}
-
-# 選択肢
-- 正解: 「回答者の回答」は「問題の答え」を正しく表しており、要素を欠落していない。
-- 説明不足: 「回答者の回答」は「問題の答え」を正しく表してはいるが、一部要素を欠落している。
-- 不正解: 「回答者の回答」は「問題の答え」を正しく表していない。
-
-# 出力
-「問題の答え」に対する「回答者の回答」について、選択肢から1つ選び、それのみを出力する。
-${body.debugMode ? "その理由も共に答える" :  ""}
- `
-    await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt,
-        temperature: 0,
-        top_p: 1,
-        max_tokens: 1000,
-        frequency_penalty: 0.0,
-        presence_penalty: 0.0,
-    }).then(e => { 
-        const text = e.data.choices[0].text
-        if(text){
-            res.status(200).json({answer:text})
-        } else {
-            res.status(500).json("OpenAI API Error")
-        }
-       
+    const body = RequestBody.parse(JSON.parse(req.body));
+    await checkAnswerIsCorrect(req.query.story, body).then(text => {
+        res.status(200).json({answer:text})
     }).catch((e) => {
         console.error(e);
-        res.status(500).json({error: "OpenAI API Error"})
+        res.status(500).json("OpenAI API Error")
     })
 }
 
--- src/pages/api/stories/[story]/question.ts
+++ src/pages/api/stories/[story]/question.ts
@@ -1,54 +1,20 @@
-import { getStory } from '@/models/getStory';
+import { askQuestion } from '@/usecases/question';
 import type { NextApiHandler } from 'next'
-import { Configuration, OpenAIApi } from "openai";
 
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
+import * as z from "zod"
+
+const RequestBody = z.object({
+    prompt: z.string(),
+    debugMode: z.boolean()
+})
 
 const handler : NextApiHandler = async (req, res) => {
     if(!(typeof req.query.story === "string")){
         throw new Error("Invalid query")
     };
-    const story = await getStory(req.query.story);
-    const body = JSON.parse(req.body);
-    const prompt = `
-# 概要
-問題とそれの答え、及びそれを読んだ回答者の質問を見て選択肢の中から出力を決めてください。
-ある程度抽象的に考えてください。
-
-# 問題
-${story.question}
-
-# 問題の答え
-${story.coreDescription}
-
-# 補足説明
-${story.additionalDescription}
-
-# 回答者の質問
-${body.prompt}
-
-# 選択肢
-- はい: 「回答者の質問」に対して「問題の答え」の状況は真である
-- いいえ: 「回答者の質問」に対して「問題の答え」の状況は偽である
-- 関係ない: 「回答者の回答」は「問題の答え」に対して関係ないもしくは読み取れないことを言っている。
-
-# 出力
-「問題の答え」を読んだ上で「回答者の質問」について、選択肢から1つ選び、それのみを回答する。
-${body.debugMode ? "その理由も共に答える" :  ""}
-`
-    await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt,
-        temperature: 0,
-        top_p: 1,
-        max_tokens: 1000,
-        frequency_penalty: 0.0,
-        presence_penalty: 0.0,
-    }).then(e => {
-        res.status(200).json({answer:e.data.choices[0].text})
+    const body = RequestBody.parse(JSON.parse(req.body));
+    return askQuestion(req.query.story, body).then(answer => {
+        res.status(200).json({answer})
     }).catch(() => {
         res.status(500).json({error: "OpenAI API Error"})
     })
